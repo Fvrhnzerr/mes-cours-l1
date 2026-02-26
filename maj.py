997ce1da6b946e01801176dd87d6172205aa57da
Fritoman-alt <batik_nitrite_0@icloud.com>
2026-02-26 22:32:53 +0300

    📚 Mise à jour du drive - 26/02/2026 à 22:32 (Avec Auto-Sync 🔄)

--- maj.py
+++ maj.py
@@ -32,7 +32,7 @@ NOM_REPO                 = "mes-cours-l1"
 MODE_MAINTENANCE = False
 
 # 🔓 LE BOUTON MOT DE PASSE (True = Code requis / False = Accès direct)
-MOT_DE_PASSE_ACTIF = True
+MOT_DE_PASSE_ACTIF = False
 
 
 # 🔐 IDENTIFIANTS (La correction magique est ici !)
